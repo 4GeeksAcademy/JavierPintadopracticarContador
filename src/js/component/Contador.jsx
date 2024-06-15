14adc2cbb93486941207f214bfdfddbe61edc339
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-06-15 11:25:10 +0000

    m2

--- src/js/component/Contador.jsx
+++ src/js/component/Contador.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-export const Contador = ({numero}) => <div className="numero"> {numero}  </div>
-
-
 //  Tenemos una constante contador que hace una funcion ({})
 //  que recibe un numero y devuelve => 
 //  un div con el numero.
+
+export const Contador = ({numero}) => <div className="numero"> {numero}  </div>
+
+
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,10 +4,10 @@ import {Contador} from "./Contador.jsx";
 //create your first component
 const Home = () => {
 	
-	// El estado se ejecuta cada vez la pagina acarga y comienza en 0.
+	// El estado se ejecuta cada vez la pagina carga y comienza en (0).
 	const [tiempo, setTiempo] = useState(0);
 
-	//Cada vez que el estado sea modificado se va a ejecutar useEfectt 
+	//Cada vez que el estado sea modificado se va a ejecutar useEfectt. 
 	// Necesito que cuando la pagina cargue comience a contar los segundos por lo que 
 	// utilizamos una funcios de js settimeout dentro de useEfect.
 	// SetTimeout ejecuta una funcion con la cantidad de milisegundos que le digamos 1seg = 1000 miliseg.
@@ -27,9 +27,10 @@ const Home = () => {
 	// Despues de Math.floor %10 le decimos que cuando llegue a 9 vuelva 0.
 	// En el siguiente dividimos tiempo /10 y añadimos %10 para que de 9 vuelva a 0.
 	// Sigue la logica en los demas.
+	// Creamos tres botones onclick mx-1 hace que separe los botones
 	return (
 		<div className="text-center">
-			<div className="caja-contador">
+			<div className="caja-contador mt-5">
 			<Contador numero = {<span className="fa fa-bomb"></span>}/>
 			<Contador numero = {Math.floor(tiempo/100000)%10}/>
 			<Contador numero = {Math.floor(tiempo/10000)%10}/>
@@ -37,6 +38,15 @@ const Home = () => {
 			<Contador numero = {Math.floor(tiempo/100)%10}/>
 			<Contador numero = {Math.floor(tiempo/10)%10}/>
 			<Contador numero = {Math.floor(tiempo%10)}/>
+			</div>
+			
+			<div className="container text-center mt-3">
+				<h2>Control Contador</h2>
+				<div className="container text-center mt-3">
+					<button className="mx-1 btn btn-success"> Inicio     </button>
+					<button className="mx-1 btn btn-danger">  Parar      </button>
+					<button className="mx-1 btn btn-warning"> Reiniciar  </button>
+				</div>
 			</div>
 		</div>
 	);
