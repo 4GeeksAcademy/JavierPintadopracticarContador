fbb10fd4c25be3517cef7a3a2b8f3b565c350ae5
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-06-15 10:57:43 +0000

    javi

---
+++ src/js/component/Contador.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Contador = ({numero}) => <div className="numero"> {numero}  </div>
+
+
+//  Tenemos una constante contador que hace una funcion ({})
+//  que recibe un numero y devuelve => 
+//  un div con el numero.
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import {Contador} from "./Contador.jsx";
 //create your first component
 const Home = () => {
+	
+	// El estado se ejecuta cada vez la pagina acarga y comienza en 0.
+	const [tiempo, setTiempo] = useState(0);
+
+	//Cada vez que el estado sea modificado se va a ejecutar useEfectt 
+	// Necesito que cuando la pagina cargue comience a contar los segundos por lo que 
+	// utilizamos una funcios de js settimeout dentro de useEfect.
+	// SetTimeout ejecuta una funcion con la cantidad de milisegundos que le digamos 1seg = 1000 miliseg.
+	// Tenemos que modificar el estado a traves de setTiempo y le damos valor + 1 para que cuente de 1 en 1.
+	// Aray de dependencias
+	useEffect( () => {
+		setTimeout( ()=>{
+			setTiempo(valor=> valor+1)
+		}, 1000 )
+	}, [tiempo] );
+	
+
+
+	// Pasamos la variable tiempo.
+	// Necesitamos que cada digito muestre su numero en orden asi que comenzamos del numero mas pequeño(ultima funcion) y lo crcemos.
+	// Utilizamos la funcion de js Math.floor que va a devolver el numero entero menor: 0.1>0 0.9>0 10.1>10
+	// Despues de Math.floor %10 le decimos que cuando llegue a 9 vuelva 0.
+	// En el siguiente dividimos tiempo /10 y añadimos %10 para que de 9 vuelva a 0.
+	// Sigue la logica en los demas.
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="caja-contador">
+			<Contador numero = {<span className="fa fa-bomb"></span>}/>
+			<Contador numero = {Math.floor(tiempo/100000)%10}/>
+			<Contador numero = {Math.floor(tiempo/10000)%10}/>
+			<Contador numero = {Math.floor(tiempo/1000)%10}/>
+			<Contador numero = {Math.floor(tiempo/100)%10}/>
+			<Contador numero = {Math.floor(tiempo/10)%10}/>
+			<Contador numero = {Math.floor(tiempo%10)}/>
+			</div>
 		</div>
 	);
 };
